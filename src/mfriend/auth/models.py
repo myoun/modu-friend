be83f7a10295d5654a6093577c573305275e1765
myoun <aiden080605@gmail.com>
2023-09-09 17:08:12 +0900

    refactor: Friend와 FriendPersonality 모델을 mfriend.ai로 옮김

--- src/mfriend/auth/models.py
+++ src/mfriend/auth/models.py
@@ -1,9 +1,8 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, Mapped, mapped_column
-from typing import Literal, List
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship, Mapped
+from typing import List
+from mfriend.ai.model import Friend
 from mfriend.database import Base
-
-Personalities = Literal["bright", "sad"]
 
 class User(Base):
     __tablename__ = "MODU_USERS"
@@ -13,21 +12,3 @@ class User(Base):
     hashed_password = Column(String(64))
 
     friends: Mapped[List["Friend"]] = relationship()
-    
-
-class Friend(Base):
-    __tablename__ = "MODU_FRIENDS"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(16))
-
-    friend: Mapped[String] = mapped_column(ForeignKey("MODU_USERS.id"))
-    personalities: Mapped[List["FriendPersonality"]] = relationship()
-
-class FriendPersonality(Base):
-    __tablename__ = "MODU_FRIEND_PERSONALITIES"
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    personality = Column(String(32))
-    
-    friend_id: Mapped[Integer] = mapped_column(ForeignKey("MODU_FRIENDS.id"))
